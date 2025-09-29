e994059f5ef1d6494af6d9141f1ba557a219f939
MedinaPiRRy <medina.khe@gmail.com>
2025-09-29 15:39:21 -0400

    [Fix] Added apiBaseUrl to dev environment on script

--- DavendWebApp/scripts/generate-env.mjs
+++ DavendWebApp/scripts/generate-env.mjs
@@ -63,6 +63,7 @@ export const environment = {
   nodeEnv: '${nodeEnv}',
   port: '${port}',
   frontendUrl: '${frontendUrl}',
+  apiBaseUrl: '${apiBaseUrl}',
 
   // Stripe
   stripeSecretKey: '${stripeSecretKey}',
