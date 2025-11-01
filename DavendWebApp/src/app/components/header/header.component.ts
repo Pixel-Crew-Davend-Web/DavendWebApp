3d5cc27249b449b4c7cc4ea1d9bc6b3b9ff31636
Mustafa <Mustafakhan614.mk@gmail.com>
2025-11-01 18:06:47 -0400

    Finalize mobile UI and responsive site fixes"

--- DavendWebApp/src/app/components/header/header.component.ts
+++ DavendWebApp/src/app/components/header/header.component.ts
@@ -3,16 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: [
+    './header.component.css',
+    './header.mobile.css'
+  ]
 })
 export class HeaderComponent implements OnInit {
 
   page: string = "/";
-
+  menuOpen = false;
   constructor() {}
 
   ngOnInit(): void {
     const urlPath = window.location.pathname.toLowerCase();
     this.page = urlPath.split('/')[1] || 'home';
   }
+
+toggleMenu() {
+  this.menuOpen = !this.menuOpen;
+  const controls = document.querySelector('.mobile-controls');
+  if (controls) {
+    if (this.menuOpen) controls.classList.add('menu-active');
+    else controls.classList.remove('menu-active');
+  }
 }
+}
+
